ee0da30f0dd84cbf8d77ea2da6f94c0fd3185892
Atila Fassina <atila@fassina.eu>
2024-11-25 22:13:00 +0100

    deno-deploy

--- src/routes/(root)/index.tsx
+++ src/routes/(root)/index.tsx
@@ -6,7 +6,13 @@ export default function Sockets() {
   return (
     <div class="grid place-items-center pt-10">
       <h1 class="text-md text-neutral-500 font-bold pb-2">SolidChat</h1>
-      <WebSocketClient initialSocketUrl="ws://localhost:3000/ws" />
+      <WebSocketClient
+        initialSocketUrl={
+          import.meta.env.DEV
+            ? "ws://localhost:3000/ws"
+            : "wss://web-sockets.deno.dev/ws"
+        }
+      />
     </div>
   );
 }
